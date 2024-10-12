f0d6cb054e39c0c79a72edc9d681f5ca7b97d323
soumya <soumya.behera@geeksforgeeks.org>
2024-10-12 19:19:20 +0530

    code refactor

--- TIC TAC TOE/game.py
+++ TIC TAC TOE/game.py
@@ -10,32 +10,54 @@ class TicTacToe:
         self.initialize_game()
     
     def initialize_game(self):
-        for i in range(2):
-            choose_piece = input(f"CHoose piece for player{i}: ")
-            if choose_piece=="X":
-                piece_type = PlayingPieceX()
-            else:
-                piece_type = PlayingPieceY()
-            new_player = Player(f"player{i}",piece_type)
-            self.players.append(new_player)
+        has = {"X":False,"Y":False}
+        flag = False
+        while not flag:
+            choose_piece = input(f"Choose piece for player{1}: from X or Y: ")
+            if choose_piece not in ["X","Y"]:
+                print("Wrong choice. Choose again")
+                continue
+            flag = True
             
+        if choose_piece=="X":
+            piece_type = PlayingPieceX()
+        else:
+            piece_type = PlayingPieceY()
+        new_player = Player(f"player{1}",piece_type)
+        self.players.append(new_player)
+        has[choose_piece] = True
+        
+        if not has["Y"]:
+            piece_type = PlayingPieceY()
+        else:
+            piece_type = PlayingPieceX()
+        new_player = Player("player2",piece_type)
+        self.players.append(new_player)
         self.game_board = Board(3)
         
+        
+    def is_valid_move(self,i,j):
+        if 0<=i<3 and 0<=j<=3:
+            return True
+        return False
         
     def start_game(self):
         no_winnwer = True
         
         while no_winnwer:
-            player = self.players.popleft()
-            
             self.game_board.print_board()
-            
             free_spaces = self.game_board.get_free_cells()
             if not free_spaces:
                 no_winnwer = False
                 break
-            
             i,j = map(int,input("Please Enter row col by a single space: ").split())
+            
+            if not self.is_valid_move(i,j):
+                print("Incorect position choosen. Choose again")
+                continue
+            
+            player = self.players.popleft()
+            
             if self.game_board.board[i][j]!=0:
                 print("Incorect position choosen. Choose again")
                 continue
@@ -52,21 +74,25 @@ class TicTacToe:
     def is_winner_found(self,row,col,playing_piece):
         row_match = col_match = digonal_match = anti_digonal_match = True
         
+        # row match
         for i in range(self.game_board.size):
             if self.game_board.board[row][i]==0 or self.game_board.board[row][i].piece_type!=playing_piece.piece_type:
                 row_match = False
                 break
             
+        # col match
         for i in range(self.game_board.size):
             if self.game_board.board[i][col]==0 or self.game_board.board[i][col].piece_type!=playing_piece.piece_type:
                 col_match = False
                 break
             
+        # digonal match
         for i in range(self.game_board.size):
             if self.game_board.board[i][i]==0 or self.game_board.board[i][i].piece_type!=playing_piece.piece_type:
                 digonal_match = False
                 break
             
+        # anti digonal match
         for i in range(self.game_board.size-1,-1,-1):
             if self.game_board.board[self.game_board.size-i][i]==0 or self.game_board.board[self.game_board.size-i][i].piece_type!=playing_piece.piece_type:
                 anti_digonal_match = False
